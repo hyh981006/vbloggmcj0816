9ddde0e3aa5d475908aae730ed3af346291c1e23
hyh981006 <2813842071@qq.com>
2022-11-14 15:10:52 +0800

    Site updated: 2022-11-14 15:10:36

---
+++ js/anzhiyufunction.js
@@ -0,0 +1,12 @@
+// 音乐绑定事件
+function musicBindEvent() {
+    document.querySelector("#nav-music .aplayer-music").addEventListener("click", function () {
+      anzhiyu.musicTelescopic();
+    });
+  }
+
+  const metingJs = document.querySelector("#nav-music meting-js");
+//判断是否是音乐
+if (metingJs.contains(event.target)) {
+} else {
+}
--- js/custom_mg/essay.js
+++ js/custom_mg/essay.js
@@ -1,4 +1,7 @@
 var percentFlag = false; // 节流阀
+var anzhiyu_musicPlaying = false;
+var anzhiyu_musicStretch = false;
+var anzhiyu_musicFirst = false;
 function essayScroll() {
   let a = document.documentElement.scrollTop || window.pageYOffset; // 卷去高度
   const waterfallResult = a % document.documentElement.clientHeight; // 卷去一个视口
@@ -90,7 +93,63 @@ var anzhiyu = {
       n.setSelectionRange(-1, -1),
       document.getElementById("comment-tips") && document.getElementById("comment-tips").classList.add("show");
   },
+  //切换音乐播放状态
+  musicToggle: function () {
+    if (!anzhiyu_musicFirst) {
+      musicBindEvent();
+      anzhiyu_musicFirst = true;
+    }
+    let msgPlay = '<i class="fa-solid fa-play"></i><span>播放音乐</span>'; // 此處可以更改為你想要顯示的文字
+    let msgPause = '<i class="fa-solid fa-pause"></i><span>暂停音乐</span>'; // 同上，但兩處均不建議更改
+    if (anzhiyu_musicPlaying) {
+      document.querySelector("#nav-music").classList.remove("playing");
+      document.getElementById("menu-music-toggle").innerHTML = msgPlay;
+      document.getElementById("nav-music-hoverTips").innerHTML = "音乐已暂停";
+      anzhiyu_musicPlaying = false;
+      document.querySelector("#nav-music").classList.remove("stretch");
+      anzhiyu_musicStretch = false;
+    } else {
+      document.querySelector("#nav-music").classList.add("playing");
+      document.getElementById("menu-music-toggle").innerHTML = msgPause;
+      document.getElementById("nav-music-hoverTips").innerHTML = "音乐开启";
+      anzhiyu_musicPlaying = true;
+      document.querySelector("#nav-music").classList.add("stretch");
+      anzhiyu_musicStretch = true;
+    }
+    document.querySelector("meting-js").aplayer.toggle();
+  },
+  // 音乐伸缩
+  musicTelescopic: function () {
+    if (anzhiyu_musicStretch) {
+      document.querySelector("#nav-music").classList.remove("stretch");
+      anzhiyu_musicStretch = false;
+    } else {
+      document.querySelector("#nav-music").classList.add("stretch");
+      anzhiyu_musicStretch = true;
+    }
+  },
+
+  //音乐上一曲
+  musicSkipBack: function () {
+    document.querySelector("meting-js").aplayer.skipBack();
+  },
+
+  //音乐下一曲
+  musicSkipForward: function () {
+    document.querySelector("meting-js").aplayer.skipForward();
+  },
+
+  //获取音乐中的名称
+  musicGetName: function () {
+    var x = $(".aplayer-title");
+    var arr = [];
+    for (var i = x.length - 1; i >= 0; i--) {
+      arr[i] = x[i].innerText;
+    }
+    return arr[0];
+  },
 };
 
 anzhiyu.changeTimeInEssay();
 anzhiyu.reflashEssayWaterFall();
+addRightMenuClickEvent();
