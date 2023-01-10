8c56ac358cfa82b3d3a7d1643723c9c486c26361
hyh981006 <2813842071@qq.com>
2023-01-10 15:06:29 +0800

    Site updated: 2023-01-10 15:06:16

--- js/custom_mg/fps.js
+++ js/custom_mg/fps.js
@@ -114,6 +114,3 @@ var newYear = () => {
 // 开了Pjax的用以下两句
 document.addEventListener('pjax:complete', newYear);
 document.addEventListener('DOMContentLoaded', newYear);
-document.onkeydown = function (e) {
-    if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) return btf.snackbarShow("别想了，我猜到了！😜已默认禁用F12！🍔"), event.keyCode = 0, event.returnValue = !1, !1
-};
